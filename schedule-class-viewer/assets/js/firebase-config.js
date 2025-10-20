7bf80876687ef010b58b0071aad968f6c3753b00
johntyrontanggaan682 <tyrontanggaan682@gmail.com>
2025-10-21 00:11:40 +0800

    made some changes firebase

--- schedule-class-viewer/assets/js/firebase-config.js
+++ schedule-class-viewer/assets/js/firebase-config.js
@@ -1,4 +1,3 @@
-// assets/js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getAuth }        from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 import { getFirestore }   from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
@@ -17,5 +16,4 @@ const app  = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db   = getFirestore(app);
 
-// expose to other modules
 window.__FB = { app, auth, db };
